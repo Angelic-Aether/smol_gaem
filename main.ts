7bc64cc3996daa484ae3b3093d9ec85477b9efe2
RatedRemoDemo <86170310+RatedRemoDemo@users.noreply.github.com>
2021-07-20 15:29:21 +1000

    properly transformed it into a small drawing program, and I may change it into other things in the future

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 input.onButtonPressed(Button.A, function () {
     turtle.forward(1)
-    music.playTone(4, music.beat(BeatFraction.Whole))
+})
+input.onPinPressed(TouchPin.P2, function () {
+    turtle.pen(TurtlePenMode.Down)
+    music.playTone(262, music.beat(BeatFraction.Quarter))
 })
 input.onButtonPressed(Button.AB, function () {
-    if (true) {
-        turtle.pen(TurtlePenMode.Up)
-    } else {
-        turtle.pen(TurtlePenMode.Down)
-    }
+    turtle.home()
+    music.playTone(523, music.beat(BeatFraction.Quarter))
 })
 input.onButtonPressed(Button.B, function () {
     turtle.turnRight()
 })
-turtle.setPosition(2, 2)
-turtle.pen(TurtlePenMode.Down)
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P1, function () {
+    turtle.pen(TurtlePenMode.Up)
+    music.playTone(349, music.beat(BeatFraction.Quarter))
 })
+turtle.setPosition(2, 2)
